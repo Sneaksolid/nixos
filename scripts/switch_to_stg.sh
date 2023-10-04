567d9fe3ddf73c48e4606a6c68b574411cb88cae
Mathias Kahr <m.kahr@trever.io>
2023-10-04 11:18:22 +0200

    stg sso login

--- scripts/switch_to_stg.sh
+++ scripts/switch_to_stg.sh
@@ -2,8 +2,6 @@ export OLD_PS1=$PS1
 export PS1="(STG) $PS1"
 export KUBECONFIG=$HOME/.kube/config_stg
 
-kubectl config set-context trever-stg > /dev/null
-
 function revert() {
     export PS1=$OLD_PS1
     unset KUBECONFIG
