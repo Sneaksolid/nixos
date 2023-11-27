a928f4a412528233a3a9df9b91863d29489b0df5
Mathias Kahr <m.kahr@trever.io>
2023-11-27 08:10:51 +0100

    update

--- modules/base.nix
+++ modules/base.nix
@@ -87,6 +87,10 @@
   xdg = {
     portal = {
       enable = true;
+      config = {
+        common.default = "*";
+      };
+
       extraPortals = with pkgs; [
         xdg-desktop-portal-wlr
         xdg-desktop-portal-gtk
